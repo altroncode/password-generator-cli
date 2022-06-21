880d62b59fbfefe2e2b99bf11f3d1f4eefcfe207
altcode <cosmosx1328@gmail.com>
2022-06-21 19:49:23 +0600

    🔧 Add conditional in setters and getters
    Add check value on None.

--- config/data.py
+++ config/data.py
@@ -13,12 +13,13 @@ class PasswordGeneratorData:
     config.read(config_path)
 
     @property
-    def email(self):
+    def email(self) -> str | None:
         return self.config.get('password_generator', 'email')
 
     @email.setter
-    def email(self, value):
-        self.config.set('password_generator', 'email', value)
+    def email(self, value: str | None):
+        if value is not None:
+            self.config.set('password_generator', 'email', value)
 
     def save(self):
         with open(self.config_path, 'w') as file:
@@ -33,28 +34,35 @@ class TelegramData:
     config.read(config_path)
 
     @property
-    def user_id(self) -> int:
-        return int(self.config.get('telegram', 'user_id'))
+    def user_id(self) -> int | None:
+        _user_id = self.config.get('telegram', 'user_id')
+        if _user_id is not None:
+            return int(_user_id)
 
     @user_id.setter
-    def user_id(self, value: str):
-        self.config.set('telegram', 'user_id', value)
+    def user_id(self, value: str | None):
+        if value is not None:
+            self.config.set('telegram', 'user_id', value)
 
     @property
-    def token(self) -> str:
+    def token(self) -> str | None:
         return self.config.get('telegram', 'token')
 
     @token.setter
-    def token(self, value: str):
-        self.config.set('telegram', 'token', value)
+    def token(self, value: str | None):
+        if value is not None:
+            self.config.set('telegram', 'token', value)
 
     @property
-    def last_message_id(self) -> int:
-        return int(self.config.get('telegram', 'last_message_id'))
+    def last_message_id(self) -> int | None:
+        _last_message_id = self.config.get('telegram', 'last_message_id')
+        if _last_message_id:
+            return int(_last_message_id)
 
     @last_message_id.setter
-    def last_message_id(self, value: str):
-        self.config.set('telegram', 'last_message_id', value)
+    def last_message_id(self, value: str | None):
+        if value is not None:
+            self.config.set('telegram', 'last_message_id', value)
 
     def save(self):
         with open(self.config_path, 'w') as file:
--- config/settings.py
+++ config/settings.py
@@ -6,50 +6,54 @@ import os
 
 
 class PasswordSettings:
-
     config = configparser.ConfigParser()
     config_path = pathlib.Path(os.curdir) / 'config.ini'
     config.read(config_path)
 
     @property
-    def default_length(self) -> str:
+    def default_length(self) -> str | None:
         return self.config.get('password', 'default_length')
 
     @default_length.setter
-    def default_length(self, value: str):
-        self.config.set('password', 'default_length', value)
+    def default_length(self, value: str | None):
+        if value is not None:
+            self.config.set('password', 'default_length', value)
 
     @property
-    def digits_in_password(self) -> str:
+    def digits_in_password(self) -> str | None:
         return self.config.get('password', 'digits_in_password')
 
     @digits_in_password.setter
-    def digits_in_password(self, value: str):
-        self.config.set('password', 'digits_in_password', value)
+    def digits_in_password(self, value: str | None):
+        if value is not None:
+            self.config.set('password', 'digits_in_password', value)
 
     @property
-    def capital_letters_in_password(self) -> str:
+    def capital_letters_in_password(self) -> str | None:
         return self.config.get('password', 'capital_letters_in_password')
 
     @capital_letters_in_password.setter
-    def capital_letters_in_password(self, value: str):
-        self.config.set('password', 'capital_letters_in_password', value)
+    def capital_letters_in_password(self, value: str | None):
+        if value is not None:
+            self.config.set('password', 'capital_letters_in_password', value)
 
     @property
-    def small_letters_in_password(self) -> str:
+    def small_letters_in_password(self) -> str | None:
         return self.config.get('password', 'small_letters_in_password')
 
     @small_letters_in_password.setter
-    def small_letters_in_password(self, value: str):
-        self.config.set('password', 'small_letters_in_password', value)
+    def small_letters_in_password(self, value: str | None):
+        if value is not None:
+            self.config.set('password', 'small_letters_in_password', value)
 
     @property
-    def punctuation_in_password(self) -> str:
+    def punctuation_in_password(self) -> str | None | None:
         return self.config.get('password', 'punctuation_in_password')
 
     @punctuation_in_password.setter
-    def punctuation_in_password(self, value: str):
-        self.config.set('password', 'punctuation_in_password', value)
+    def punctuation_in_password(self, value: str | None):
+        if value is not None:
+            self.config.set('password', 'punctuation_in_password', value)
 
     def save(self):
         with open(self.config_path, 'w') as file:
