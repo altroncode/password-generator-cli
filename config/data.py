b811c117208f89d17dfb65a577cbfc3ea3eab55d
altcode <cosmosx1328@gmail.com>
2022-06-21 17:38:55 +0600

    🔧 Move fields in data.py to __init__()

--- config/data.py
+++ config/data.py
@@ -5,20 +5,25 @@ import os
 import pathlib
 
 
-config = configparser.ConfigParser()
-config_path = pathlib.Path(os.curdir) / 'config.ini'
-config.read(config_path)
-
-
 class PasswordGeneratorData:
     """Data for the whole project"""
 
-    email = config.get('password_generator', 'email')
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path(os.curdir) / 'config.ini'
+    config.read(config_path)
+
+    def __init__(self):
+        self.email = self.config.get('password_generator', 'email')
 
 
 class TelegramData:
     """Data for work with telegram"""
 
-    user_id = int(config.get('telegram', 'user_id'))
-    token = config.get('telegram', 'token')
-    last_message_id = int(config.get('telegram', 'last_message_id'))
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path(os.curdir) / 'config.ini'
+    config.read(config_path)
+
+    def __init__(self):
+        self.user_id = int(self.config.get('telegram', 'user_id'))
+        self.token = self.config.get('telegram', 'token')
+        self.last_message_id = int(self.config.get('telegram', 'last_message_id'))
