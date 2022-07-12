ca055f2703f341c777462d8a943bf53de23ef71d
altcode <cosmosx1328@gmail.com>
2022-07-12 20:27:44 +0600

    :recycle: Rename `data_source` to `settings_source` in __main__.py

--- __main__.py
+++ __main__.py
@@ -13,9 +13,9 @@ import cli
 
 arguments = cli.get_arguments(argument_parser=cli.parser, args=sys.argv[1:])
 
-data_source = data_sources.CLIArgumentsDataSource(arguments) + data_sources.IniDataSource(settings.SETTINGS_PATH)
-password_settings = settings.PasswordSettings(source=data_source)
-general_settings = settings.GeneralSettings(source=data_source)
+settings_source = data_sources.CLIArgumentsDataSource(arguments) + data_sources.IniDataSource(settings.SETTINGS_PATH)
+password_settings = settings.PasswordSettings(source=settings_source)
+general_settings = settings.GeneralSettings(source=settings_source)
 password = arguments.password or password.Password(settings=password_settings)
 
 print(password)
