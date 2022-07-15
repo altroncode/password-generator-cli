a5bf16207651a8ba958d3c0847dc642d28747bff
altcode <cosmosx1328@gmail.com>
2022-07-16 02:24:09 +0600

    :recycle: Add styles to `TelegramPasswordInfoBuilder`

--- password_info.py
+++ password_info.py
@@ -30,22 +30,22 @@ class TelegramPasswordInfoBuilder:
         return self._password_info
 
     def set_platform(self, platform: str) -> str:
-        self._password_info += f'\nPlatform: {platform}'
+        self._password_info += f'\n*Platform*: {platform}'
         self._password_info.lstrip('\n')
         return self._password_info
 
     def set_username(self, username: str) -> str:
-        self._password_info += f'\nUsername: {username}'
+        self._password_info += f'\n*Username*: {username}'
         self._password_info.lstrip('\n')
         return self._password_info
 
     def set_email(self, email: str) -> str:
-        self._password_info += f'\nEmail: {email}'
+        self._password_info += f'\n*Emails*: {email}'
         self._password_info.lstrip('\n')
         return self._password_info
 
     def set_note(self, note: str) -> str:
-        self._password_info += f'\nNote: {note}'
+        self._password_info += f'\n*Note*: {note}'
         self._password_info.lstrip('\n')
         return self._password_info
 
