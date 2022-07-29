0e97ca3226dcb2b87e419643835954d9b0d319e6
altcode <cosmosx1328@gmail.com>
2022-07-29 20:03:29 +0600

    :recycle: Edit create_password and _get_symbols in Password
    Add a guarantee that the password will contain all kinds of
    characters specified by the user.

--- password.py
+++ password.py
@@ -1,3 +1,4 @@
+import random
 import secrets
 import string
 
@@ -11,24 +12,33 @@ class Password:
         self._settings = settings
         self._password = self.create_password()
 
-    def _get_symbols(self) -> str:
-        symbols = ''
-        if self._settings.is_digits:
-            symbols += string.digits
-        if self._settings.is_capital_letters:
-            symbols += string.ascii_uppercase
-        if self._settings.is_small_letters:
-            symbols += string.ascii_lowercase
-        if self._settings.is_punctuation:
-            symbols += string.punctuation
-        return symbols
-
     def create_password(self) -> str:
-        symbols = self._get_symbols()
-        return "".join(secrets.choice(symbols) for _ in range(self._settings.length))
+        length: int = self._settings.length
+        next_symbol: int = 0
+        password_symbols: list[str] = [''] * 30
+
+        for i, symbols in enumerate(self._get_symbols()):
+            password_symbols[next_symbol:] = [
+                secrets.choice(symbols) for _ in range(length - next_symbol)
+            ]
+            next_symbol = random.randint(i+1, length-1)
+        random.shuffle(password_symbols)
+        return ''.join(password_symbols)
+
+    def _get_symbols(self):
+        symbols: dict = {
+            string.digits: self._settings.is_digits,
+            string.ascii_uppercase: self._settings.is_capital_letters,
+            string.ascii_lowercase: self._settings.is_small_letters,
+            string.punctuation: self._settings.is_punctuation
+        }
+        return {key: value for key, value in symbols.items() if value}
 
     def __str__(self):
         return self._password
+
+    def __repr__(self):
+        return f'{type(self)}(settings={self._settings})'
 
     def __len__(self):
         return len(self._password)
