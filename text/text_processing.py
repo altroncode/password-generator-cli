f34cd97a8b393d9348e4fca04f7a0ae8e5428a02
altcode <cosmosx1328@gmail.com>
2022-11-16 00:05:23 +0600

    :sparkles: Add format_text and color_text

--- text/text_processing.py
+++ text/text_processing.py
@@ -5,13 +5,19 @@ class BaseTextProcessing(abc.ABC):
     def escape_text(self, text: str) -> str:
         pass
 
-    def format_text(self, text: str) -> str:
+    def format_text(self, text: str, formatting: str) -> str:
         pass
 
-    def color_text(self, text: str) -> str:
+    def color_text(self, text: str, color: str) -> str:
         pass
 
 
 class SimpleTextProcessing(BaseTextProcessing):
     def escape_text(self, text: str) -> str:
         return ''.join(f'\\{symbol}' for symbol in text)
+
+    def color_text(self, text: str, color: str) -> str:
+        return f'{color}{text}\x1b[0m'
+
+    def format_text(self, text: str, formatting: str) -> str:
+        return text
