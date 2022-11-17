c7bc2393d013dbde60b25c2bcf796326598e9a44
altcode <cosmosx1328@gmail.com>
2022-11-17 21:14:25 +0600

    :sparkles: Create TelegramTextProcessing

--- text/text_processing.py
+++ text/text_processing.py
@@ -1,4 +1,7 @@
 import abc
+import typing
+
+TelegramFormatting: typing.TypeAlias = typing.Literal['bold', 'code', 'pre', 'underline', 'strike']
 
 
 class BaseTextProcessing(abc.ABC):
@@ -21,3 +24,32 @@ class SimpleTextProcessing(BaseTextProcessing):
 
     def format_text(self, text: str, formatting: str) -> str:
         return text
+
+
+class TelegramTextProcessing(BaseTextProcessing):
+
+    def __init__(self):
+        self.__simple_text_processing = SimpleTextProcessing()
+
+    def escape_text(self, text: str) -> str:
+        return self.__simple_text_processing.escape_text(text)
+
+    def color_text(self, text: str, color: str) -> str:
+        return text
+
+    def format_text(self, text: str, formatting: TelegramFormatting) -> str:
+        pattern = self.__get_formatting_pattern(formatting)
+        return pattern.format(text)
+
+    def __get_formatting_pattern(self, formatting: TelegramFormatting) -> str:
+        tags = self.__get_formatting_tags()
+        tag = tags[formatting]
+        return f'<{tag}>{{}}<\\{tag}>'
+
+    @staticmethod
+    def __get_formatting_tags() -> dict[TelegramFormatting: str]:
+        return {
+            'bold': 'b', 'italic': 'i',
+            'underline': 'u', 'strike': 's',
+            'code': 'code', 'pre': 'pre'
+        }
