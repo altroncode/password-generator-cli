22ec958259f9638d88deedd6e600b49494c3dde4
altcode <cosmosx1328@gmail.com>
2022-11-17 22:59:54 +0600

    :recycle: Rename arguments

--- cli.py
+++ cli.py
@@ -10,12 +10,12 @@ parser.add_argument('--platform', '-p', type=str, dest='platform', nargs='?')
 parser.add_argument('--email', '-e', type=str, dest='emails', nargs='*')
 parser.add_argument('--login', '-lg', type=str, dest='login', nargs='?')
 parser.add_argument('--storages', '-s', type=str, dest='storages', nargs='*')
-parser.add_argument('--password', '-ps', type=str, dest='password', nargs='?')
-parser.add_argument('--is_note', '-in', action='store_true', dest='is_note')
-parser.add_argument('--is_digits', '-id', type=bool, dest='is_digits')
-parser.add_argument('--is_capital_letters', '-ic', type=bool, dest='is_capital_letters')
-parser.add_argument('--is_small_letters', '-is', type=bool, dest='is_small_letters')
-parser.add_argument('--is_punctuation', '-ip', type=bool, dest='is_punctuation')
+parser.add_argument('--password', '-pw', type=str, dest='password', nargs='?')
+parser.add_argument('--is_note', '-n', action='store_true', dest='is_note')
+parser.add_argument('--is_digits', '-d', type=bool, dest='is_digits')
+parser.add_argument('--is_capital_letters', '-cl', type=bool, dest='is_capital_letters')
+parser.add_argument('--is_small_letters', '-sl', type=bool, dest='is_small_letters')
+parser.add_argument('--is_punctuation', '-pt', type=bool, dest='is_punctuation')
 
 
 def get_arguments(argument_parser: argparse.ArgumentParser, args: list[str]) -> argparse.Namespace:
