ec6b93bf9282f7916c003322fccb18e3886dd6b7
altcode <cosmosx1328@gmail.com>
2022-11-17 21:44:17 +0600

    :recycle: Rename Colors to StatusColor

--- text_processing/color.py
+++ text_processing/color.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Colors:
+class StatusColor:
     ERROR: str = random.choice(('\033[31m', '\033[91m'))
     SUCCESS: str = random.choice(('\033[32m', '\033[92m'))
     INFO: str = random.choice(('\033[34m', '\033[94m'))
