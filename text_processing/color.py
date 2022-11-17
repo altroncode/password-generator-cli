af0b88e07b8e32c730af430b6c11bc41040c9e02
altcode <cosmosx1328@gmail.com>
2022-11-17 21:49:06 +0600

    :sparkles: Create Color enum

--- text_processing/color.py
+++ text_processing/color.py
@@ -2,6 +2,16 @@ import enum
 import random
 
 
+class Color(str, enum.Enum):
+    RED: str = random.choice(('\033[31m', '\033[91m'))
+    GREEN: str = random.choice(('\033[32m', '\033[92m'))
+    BLUE: str = random.choice(('\033[34m', '\033[94m'))
+    YELLOW: str = random.choice(('\033[33m', '\033[93m'))
+
+    def __str__(self):
+        return self.value
+
+
 class StatusColor(str, enum.Enum):
     ERROR: str = random.choice(('\033[31m', '\033[91m'))
     SUCCESS: str = random.choice(('\033[32m', '\033[92m'))
