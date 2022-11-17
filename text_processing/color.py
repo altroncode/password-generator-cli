fd722d62b15078ff52b1e3aa254bc43ab52d0306
altcode <cosmosx1328@gmail.com>
2022-11-17 21:46:16 +0600

    :recycle: Make StatusColor into Enum

--- text_processing/color.py
+++ text_processing/color.py
@@ -1,8 +1,12 @@
+import enum
 import random
 
 
-class StatusColor:
+class StatusColor(str, enum.Enum):
     ERROR: str = random.choice(('\033[31m', '\033[91m'))
     SUCCESS: str = random.choice(('\033[32m', '\033[92m'))
     INFO: str = random.choice(('\033[34m', '\033[94m'))
     WARNING: str = random.choice(('\033[33m', '\033[93m'))
+
+    def __str__(self):
+        return self.value
