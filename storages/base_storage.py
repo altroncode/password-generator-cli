048ac8ad4c9a9331210870ad237eccce02ea99b9
altcode <cosmosx1328@gmail.com>
2022-12-03 17:05:10 +0600

    :recycle: Add settings to BaseStorage

--- storages/base_storage.py
+++ storages/base_storage.py
@@ -1,8 +1,12 @@
 import abc
 
+import config.data_models
+import password
+
 
 class BaseStorage(abc.ABC):
+    __settings = config.data_models.BaseModel
 
     @abc.abstractmethod
-    def keep(self, password, password_info: str) -> None:
+    def keep(self, password_: password.Password, password_info: str) -> None:
         pass
