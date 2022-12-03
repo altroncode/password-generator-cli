65d7cc0a102f0db2b9e5f23c1d2f10404a0df101
altcode <cosmosx1328@gmail.com>
2022-12-03 15:45:22 +0600

    :recycle: Use text_processing instead of utils

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 import typing
 
-import utils
+import text_processing
 from config import settings
 
 
@@ -23,22 +23,23 @@ class CredentialsSentToTelegramBuilder:
 
     def __init__(self):
         self._credentials = ''
+        self.__text_processing = text_processing.TelegramTextProcessing()
 
     def get_password_info(self) -> str:
         return self._credentials
 
     def set_platform(self, platform: str) -> str:
-        self._credentials += f'\n*Platform*: {utils.escape_message(platform)}'
+        self._credentials += f'\n*Platform*: {self.__text_processing.escape_text(platform)}'
         self._credentials.lstrip('\n')
         return self._credentials
 
     def set_login(self, login: str) -> str:
-        self._credentials += f'\n*Login*: {utils.escape_message(login)}'
+        self._credentials += f'\n*Login*: {self.__text_processing.escape_text(login)}'
         self._credentials.lstrip('\n')
         return self._credentials
 
     def set_email(self, email: str) -> str:
-        self._credentials += f'\n*Email*: {utils.escape_message(email)}'
+        self._credentials += f'\n*Email*: {self.__text_processing.escape_text(email)}'
         self._credentials.lstrip('\n')
         return self._credentials
 
