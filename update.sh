16bfd4374c9661a12634202330c57d90332762b7
altcode <cosmosx1328@gmail.com>
2023-04-14 11:12:57 +0600

    :hammer: Update update.sh

--- update.sh
+++ update.sh
@@ -3,17 +3,29 @@
 shopt -s extglob
 shopt -s dotglob
 
+RED='\033[0;31m'
+RESET='\033[0m'
+
+function update_config {
+  mv "$2" "$(dirname "$1")/$(basename "$2").old"
+}
+
 if [[ -d .git ]]; then
-  echo "Installation must not take place in the development directory. Use install.sh instead"
+  echo -e "${RED}Installation must take place in the development directory${RESET}"
   exit 1
 fi
 
-external_launch=false
+current_directory=$(dirname "$0")
+source_directory_name=$(uuidgen)
+temporary_installation_directory_name=$(uuidgen)
+config_file_path=${current_directory}/config/settings.ini
 
-while getopts "x" opt; do
+while getopts ":c" opt; do
   case ${opt} in
-  x)
-    external_launch=true
+  c)
+    old_config_path="$OPTARG"
+    update_config "$config_file_path" "$old_config_path"
+    exit 0
     ;;
   \?)
     echo -e "${RED}Wrong flag: -$OPTARG${RESET}"
@@ -22,16 +34,6 @@ while getopts "x" opt; do
   esac
 done
 
-current_directory=$(dirname "$0")
-source_directory_name=$(uuidgen)
-temporary_installation_directory_name=$(uuidgen)
-config_file_path=${current_directory}/config/settings.ini
-
-if $external_launch; then
-  new_config_path=$config_file_path
-  return
-fi
-
 chmod +x "$current_directory"/update.sh
 
 git clone https://github.com/altroncode/password-generator-cli.git "$source_directory_name"
@@ -39,13 +41,12 @@ git clone https://github.com/altroncode/password-generator-cli.git "$source_dire
 cp "$config_file_path" "$current_directory"
 
 rm -rv !("$source_directory_name"|.env|settings.ini)
+mkdir .git
 bash "$current_directory/$source_directory_name/install.sh" \
  "$current_directory/$temporary_installation_directory_name" -l
+rm -rf .git
 
-# shellcheck disable=SC1090
-source "$current_directory/$temporary_installation_directory_name/update.sh" -x
+bash "$current_directory/$temporary_installation_directory_name/update.sh" -c "$config_file_path"
 
-mv "$current_directory"/"$(basename "$config_file_path")" "$new_config_path.old"
-
-rm "$current_directory/$temporary_installation_directory_name/.env"
-mv "$current_directory/$temporary_installation_directory_name/*" "$current_directory"
+rm -rf "$current_directory/$temporary_installation_directory_name/.env"
+mv "$current_directory"/"$temporary_installation_directory_name"/* "$current_directory"
